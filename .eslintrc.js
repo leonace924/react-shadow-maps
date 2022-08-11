43b11120775a806744ff506caf8023123d282d80
leonace924 <meobius123@gmail.com>
2022-08-11 15:12:33 -0400

    feat: add eslint, prettier and update tsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
