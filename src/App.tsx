335d414d0490a88973168e12ff9af99dbb981d0c
leonace924 <meobius123@gmail.com>
2022-08-12 15:05:45 -0400

    fix: update and clean the structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import ShadowMap from 'components/ShadowMap';
 
 function App() {
   return (
-    <div className="container p-8 m-10 mx-auto bg-gray-200 border shadow rounded-xl">
-      <p className="mb-5 text-3xl font-bold text-gray-700">Welcome!</p>
-      <p className="text-lg text-black">React and Tailwind CSS in action</p>
+    <div className="container p-8 mx-auto bg-gray-200 border shadow rounded-xl">
+      <ShadowMap />
     </div>
   );
 }
